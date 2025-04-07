3b3eb6453fd933a0cddaf1f9a6900b8e16600b56
Gabriel H. Nogueira <gabrielhnogueira015@gmail.com>
2025-04-07 04:36:19 -0300

    iintrodução hyperlink para drinks

--- js/inicial.js
+++ js/inicial.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+//document.addEventListener("DOMContentLoaded", function() {
   // Placeholder para funções dos botões
-  document.querySelectorAll(".btn").forEach(function(button) {
-    button.addEventListener("click", function() {
-      alert("Funcionalidade ainda não implementada.");
-    });
-  });
-});
+  //document.querySelectorAll(".btn").forEach(function(button) {
+    //button.addEventListener("click", function() {
+      //alert("Funcionalidade ainda não implementada.");
+  //  });
+  //});
+//});
