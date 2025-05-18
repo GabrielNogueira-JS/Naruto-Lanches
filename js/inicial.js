81204a80d017c1f48a70b566bd7fd5d5dc7ef232
_N0gu3ira <gabrielhnogueira015@gmail.com>
2025-05-17 22:49:30 -0300

    home top

--- js/inicial.js
+++
@@ -1,8 +0,0 @@
-//document.addEventListener("DOMContentLoaded", function() {
-  // Placeholder para funções dos botões
-  //document.querySelectorAll(".btn").forEach(function(button) {
-    //button.addEventListener("click", function() {
-      //alert("Funcionalidade ainda não implementada.");
-  //  });
-  //});
-//});
